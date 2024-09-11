64d09bb7e1dc4ca29e7d35ef407c3c4253e77231
Aliaksandr Zahatski <zag@cpan.org>
2024-09-11 18:04:36 +0200

    clean due to refactor

--- src/utils/redirects.ts
+++
@@ -1,9 +0,0 @@
-import { PUBLIC_PATH } from "../constants";
-import * as fs from "fs"
-import { getSiteInfo } from "../utils";
-
-export function generateRedirects() {
-    const {redirects} = getSiteInfo()
-    
-  fs.writeFileSync(`${PUBLIC_PATH}/vercel.json`, JSON.stringify({redirects,"cleanUrls": true},null,2));
-}
